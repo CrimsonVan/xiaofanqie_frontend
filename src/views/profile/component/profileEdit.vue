44057d000de229e8a828be015d511c7911aed726
crimsonvan <2329016850@qq.com>
2025-01-11 05:02:51 +0800

    完成个人信息页面的编辑子组件

---
+++ src/views/profile/component/profileEdit.vue
@@ -0,0 +1,194 @@
+<template>
+  <van-popup class="popup" v-model:show="showText" position="bottom">
+    <!-- 头部操作栏 -->
+    <div class="textTop">
+      <span @click="() => (showText = false)">取消</span>
+      <span>编辑{{ hearderText }}</span>
+      <span @click="changeProfile" class="save-text">保存</span>
+    </div>
+    <!-- 输入框 or 选择框 -->
+    <div class="edit-inp">
+      <!-- 输入框 -->
+      <van-field
+        v-if="hearderText === '签名' || hearderText === '名字'"
+        v-model="inpVal"
+        rows="1"
+        autosize
+        maxlength="14"
+        type="textarea"
+        :placeholder="placeholderText"
+        class="nickname_inp"
+      />
+      <div v-if="hearderText === '生日'" class="selectBirth">
+        <span class="birth1">生日信息</span><span class="birth2">{{ birthDate }}</span>
+        <van-icon @click="() => (isShowBirth = true)" name="arrow" />
+      </div>
+      <!-- 提示语 -->
+      <div class="warning">
+        {{ warnText }}
+      </div>
+      <!-- 日期选择框 -->
+      <van-date-picker
+        v-if="isShowBirth && hearderText === '生日'"
+        v-model="currentDate"
+        title="选择日期"
+        :min-date="minDate"
+        :max-date="maxDate"
+        @confirm="confirmBirth"
+      />
+    </div>
+  </van-popup>
+</template>
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import { useNumStore } from '@/stores'
+import {
+  userUpdateNicknameService,
+  userUpdateSignatureService,
+  userUpdateBirthdayService
+} from '@/api/user'
+const useStore = useNumStore()
+const minDate = new Date(1970, 1, 1)
+const maxDate = new Date(2024, 1, 1)
+const birthDate = ref(useStore.userInfo.birthday) //生日日期
+const currentDate = ref(useStore.userInfo.birthday.split('-')) //选择器上的生日
+const hearderText = ref<string>('') //编辑弹窗标题
+const showText = ref<boolean>(false) //编辑弹框是否显示
+const inpVal = ref<string>('') //输入框内容
+const placeholderText = ref<string>('') //placeholder文字
+const warnText = ref<string>('') //提示词
+const isShowBirth = ref(false) //是否选择生日日期
+
+//编辑个人信息
+const openEditProfile = (type: string) => {
+  if (type === '名字') {
+    inpVal.value = useStore.userInfo.nick_name
+  } else if (type === '签名') {
+    inpVal.value = useStore.userInfo.signature
+  } else if (type === '生日') {
+    inpVal.value = useStore.userInfo.signature
+  }
+  hearderText.value = type
+  placeholderText.value = `请添加一个${type}`
+  showText.value = true
+}
+//修改个人信息
+const changeProfile = async () => {
+  if (hearderText.value === '名字') {
+    if (inpVal.value === '') {
+      warnText.value = '不能为空'
+      return
+    }
+    if (inpVal.value === useStore.userInfo.nick_name) {
+      warnText.value = '不能和原来名字相同'
+      return
+    }
+    await userUpdateNicknameService({
+      nickname: inpVal.value,
+      username: useStore.userInfo.username
+    })
+  } else if (hearderText.value === '签名') {
+    if (inpVal.value === '') {
+      warnText.value = '不能为空'
+      return
+    }
+    if (inpVal.value === useStore.userInfo.signature) {
+      warnText.value = '不能和原来签名相同'
+      return
+    }
+    await userUpdateSignatureService({
+      signature: inpVal.value,
+      username: useStore.userInfo.username
+    })
+  } else if (hearderText.value === '生日') {
+    if (currentDate.value.join('-') === useStore.userInfo.birthday) {
+      warnText.value = '不能和原来生日相同'
+      return
+    }
+    await userUpdateBirthdayService({
+      birthday: currentDate.value.join('-'),
+      username: useStore.userInfo.username
+    })
+  }
+  //修改完更新个人信息
+  useStore.getUserInfo(useStore.userInfo.username)
+  showText.value = false
+}
+//选定生日日期
+const confirmBirth = () => {
+  isShowBirth.value = false
+  birthDate.value = currentDate.value.join('-')
+}
+//暴露打开弹窗的方法
+defineExpose({
+  openEditProfile
+})
+watch(
+  () => inpVal.value,
+  (newVal) => {
+    if (newVal === '') {
+      warnText.value = '不能为空'
+    } else {
+      warnText.value = ''
+    }
+  }
+)
+</script>
+<style lang="scss" scoped>
+.popup {
+  background-color: #f5f5f5;
+  font-size: 17px;
+  color: #333333;
+  height: 100vh;
+  padding: 10px;
+  .textTop {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+    font-size: 15px;
+    .save-text {
+      color: var(--theme-color);
+    }
+  }
+  .edit-inp {
+    margin-top: 24px;
+    width: 100%;
+    // background-color: salmon;
+    .nickname_inp {
+      border-radius: 16px;
+      overflow: hidden;
+    }
+    .warning {
+      width: 100%;
+      font-size: 12px;
+      color: red;
+      padding: 2px 14px;
+      margin-bottom: 10px;
+    }
+    .selectBirth {
+      width: 100%;
+      height: 44px;
+      border-radius: 14px;
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      background-color: #ffffff;
+      padding: 0 13px;
+      margin-bottom: 2px;
+      .birth1 {
+        margin-left: 0px;
+      }
+      .birth2 {
+        margin-left: auto;
+
+        color: black;
+      }
+      .van-icon-arrow {
+        margin-left: 4px;
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,7 @@
 <template>
   <van-nav-bar title="编辑资料" :fixed="true">
     <template #left>
-      <van-icon name="arrow-left" @click="goback" />
+      <van-icon name="arrow-left" @click="() => router.back()" />
     </template>
   </van-nav-bar>
   <div class="safeTop"></div>
@@ -11,21 +11,20 @@
       <div class="btn">+</div>
     </van-uploader>
   </div>
-
   <div class="edit-item">
     <div class="title">名字</div>
     <div class="content">{{ useStore.userInfo.nick_name }}</div>
-    <van-icon name="arrow" @click="goChange('名字')" />
+    <van-icon name="arrow" @click="goChangeProfile('名字')" />
   </div>
   <div class="edit-item">
-    <div class="title">小红书号</div>
+    <div class="title">小番茄号</div>
     <div class="content">xhs{{ useStore.userInfo.username }}</div>
     <van-icon name="arrow" />
   </div>
   <div class="edit-item">
     <div class="title">简介</div>
     <div class="content">{{ useStore.userInfo.signature }}</div>
-    <van-icon name="arrow" @click="goChange('签名')" />
+    <van-icon name="arrow" @click="goChangeProfile('签名')" />
   </div>
   <div class="edit-item">
     <div class="title">性别</div>
@@ -40,7 +39,7 @@
   <div class="edit-item">
     <div class="title">生日</div>
     <div class="content">{{ useStore.userInfo.birthday }}</div>
-    <van-icon name="arrow" @click="goChange('生日')" />
+    <van-icon name="arrow" @click="goChangeProfile('生日')" />
   </div>
   <div class="edit-item">
     <div class="title">职业</div>
@@ -52,189 +51,35 @@
     <div class="content">中国-辽宁-白塔区</div>
     <van-icon name="arrow" />
   </div>
-  <div style="width: 100%; height: 100px; background-color: palegoldenrod">
-    <!-- <ImgCutter :imgMove="true" :boxWidth="300" @cutDown="cutDown"></ImgCutter> -->
-    <!-- <vue-cropper :src="useStore.userInfo.avatar"> </vue-cropper> -->
-  </div>
-  <van-popup class="popup" v-model:show="showText" position="bottom">
-    <div class="textTop">
-      <span @click="() => (showText = false)">取消</span>
-      <span>编辑{{ hearderText }}</span>
-      <span @click="changeProfile" class="save-text">保存</span>
-    </div>
-    <div class="edit-inp">
-      <van-field
-        v-if="hearderText === '签名' || hearderText === '名字'"
-        v-model="title"
-        rows="1"
-        autosize
-        maxlength="14"
-        type="textarea"
-        :placeholder="placeholderText"
-        class="nickname_inp"
-      />
-      <div v-if="hearderText === '生日'" class="selectBirth">
-        <span class="birth1">生日信息</span><span class="birth2">{{ birthDate }}</span>
-        <van-icon @click="() => (isShowBirth = true)" name="arrow" />
-      </div>
-      <div class="warning">
-        {{ warnText }}
-      </div>
-      <van-date-picker
-        v-if="isShowBirth && hearderText === '生日'"
-        v-model="currentDate"
-        title="选择日期"
-        :min-date="minDate"
-        :max-date="maxDate"
-        @confirm="confirmBirth"
-      />
-    </div>
-  </van-popup>
+  <!-- 子组件 -->
+  <profileEdit ref="profileEditRef"></profileEdit>
 </template>
 <script lang="ts" setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref } from 'vue'
 import { useNumStore } from '@/stores'
 import { useRouter } from 'vue-router'
 import { compressFile } from '@/utils/compress'
-import {
-  userUpdateAvatarService,
-  userUpdateNicknameService,
-  userUpdateSignatureService,
-  userUpdateBirthdayService,
-  userTestService
-} from '@/api/user'
-import { getNowFormatDate } from '@/utils/data'
+import profileEdit from './component/profileEdit.vue'
+import { userUpdateAvatarService } from '@/api/user'
 const useStore = useNumStore()
-const currentDate = ref(['2021', '01', '01'])
-const minDate = new Date(1970, 1, 1)
-const maxDate = new Date(2024, 1, 1)
-const birthDate = ref(useStore.userInfo.birthday)
-const confirmBirth = (date: any) => {
-  isShowBirth.value = false
-  birthDate.value = date.selectedValues.join('-')
-}
-const isShowBirth = ref(false)
 const router = useRouter()
-const goback = () => {
-  router.back()
-}
-const hearderText = ref('')
-const showText = ref(false)
-const title = ref('')
-const placeholderText = ref('')
-// 创建一个空对象实例
+const profileEditRef = ref<any>(null) //子组件dom
 let formData = new FormData()
+//上传头像
 const afterRead = async (file: any) => {
-  console.log('打印file', file.file)
   let res1 = await compressFile(file.file)
-  console.log('打印压缩后file', res1)
   formData.append('username', useStore.userInfo.username)
   formData.append('avatar', res1 as any)
-  let res = await userUpdateAvatarService(formData)
-  console.log('打印更新头像', res.data)
-  await useStore.getUserInfo(useStore.userInfo.username)
+  await userUpdateAvatarService(formData)
+  useStore.getUserInfo(useStore.userInfo.username)
 }
-
-const fileList = ref([])
-console.log(fileList.value)
-const goChange = (type: string) => {
-  if (type === '名字') {
-    hearderText.value = '名字'
-    title.value = useStore.userInfo.nick_name
-    placeholderText.value = '请添加一个名字'
-  }
-  if (type === '签名') {
-    hearderText.value = '签名'
-    title.value = useStore.userInfo.signature
-    placeholderText.value = '请添加一个签名'
-  }
-  if (type === '生日') {
-    hearderText.value = '生日'
-    title.value = useStore.userInfo.signature
-    placeholderText.value = '请添加一个生日'
-  }
-  showText.value = true
+// 打开子组件
+const goChangeProfile = (type: string) => {
+  profileEditRef.value.openEditProfile(type)
 }
-
-const warnText = ref('')
-const changeProfile = async () => {
-  if (hearderText.value === '名字') {
-    if (title.value === '') {
-      warnText.value = '不能为空'
-      return
-    }
-    if (title.value === useStore.userInfo.nick_name) {
-      warnText.value = '不能和原来名字相同'
-      return
-    }
-    let res: any = await userUpdateNicknameService({
-      nickname: title.value,
-      username: useStore.userInfo.username
-    })
-    console.log('打印更新昵称', res.data)
-    await useStore.getUserInfo(useStore.userInfo.username)
-    showText.value = false
-  }
-
-  if (hearderText.value === '签名') {
-    if (title.value === '') {
-      warnText.value = '不能为空'
-      return
-    }
-    if (title.value === useStore.userInfo.signature) {
-      warnText.value = '不能和原来签名相同'
-      return
-    }
-    let res: any = await userUpdateSignatureService({
-      signature: title.value,
-      username: useStore.userInfo.username
-    })
-    console.log('打印更新签名', res.data)
-    await useStore.getUserInfo(useStore.userInfo.username)
-    showText.value = false
-  }
-  if (hearderText.value === '生日') {
-    if (birthDate.value === useStore.userInfo.birthday) {
-      warnText.value = '不能和原来生日相同'
-      console.log('不能和原来生日相同')
-
-      return
-    }
-    let res: any = await userUpdateBirthdayService({
-      birthday: birthDate.value,
-      username: useStore.userInfo.username
-    })
-    console.log('打印更新生日', res.data)
-    await useStore.getUserInfo(useStore.userInfo.username)
-    showText.value = false
-  }
-}
-watch(
-  () => title.value,
-  (newVal) => {
-    // console.log('打印watch', newVal)
-    if (newVal === '') {
-      warnText.value = '不能为空'
-    } else {
-      warnText.value = ''
-    }
-  }
-)
-onMounted(async () => {
-  let res = await userTestService({
-    username: '13114209344',
-    cate_id: undefined,
-    status: '通过',
-    id: undefined,
-    pagenum: 1
-  })
-  console.log('测试', res.data.data)
-  console.log('时间', getNowFormatDate())
-})
 </script>
 <style lang="scss" scoped>
 ::v-deep(.van-nav-bar__left) {
-  //   background-color: saddlebrown;
   padding: 0 2px;
   font-size: 20px;
 }
@@ -298,62 +143,6 @@ onMounted(async () => {
     color: #ccc;
     font-size: 15px;
     margin-top: 1px;
-  }
-}
-.popup {
-  background-color: #f5f5f5;
-  font-size: 17px;
-  color: #333333;
-  height: 100vh;
-  padding: 10px;
-  .textTop {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-    font-size: 15px;
-    .save-text {
-      color: var(--theme-color);
-    }
-  }
-  .edit-inp {
-    margin-top: 24px;
-    width: 100%;
-    // background-color: salmon;
-    .nickname_inp {
-      border-radius: 16px;
-      overflow: hidden;
-    }
-    .warning {
-      width: 100%;
-      font-size: 12px;
-      color: red;
-      padding: 2px 14px;
-      margin-bottom: 10px;
-    }
-    .selectBirth {
-      width: 100%;
-      height: 44px;
-      border-radius: 14px;
-      font-size: 14px;
-      display: flex;
-      align-items: center;
-      background-color: #ffffff;
-      padding: 0 13px;
-      margin-bottom: 2px;
-      .birth1 {
-        margin-left: 0px;
-      }
-      .birth2 {
-        margin-left: auto;
-
-        color: black;
-      }
-      .van-icon-arrow {
-        margin-left: 4px;
-        margin-right: 0;
-      }
-    }
   }
 }
 </style>
